d9498f1c11e5d9eb8c04fae31c8a114a3c1ce88b
christophe <rosse.christophe@gmail.com>
2023-03-01 18:29:39 +0100

    Modif de l'injection SQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO timeplanner.utilisateur (id_user, nom, prenom, username, email) VALUES (1, 'Rossé', 'Christophe', 'crosse', 'cr@proconcept.ch');
-INSERT INTO timeplanner.utilisateur (id_user, nom, prenom, username, email) VALUES (2, 'Rossé', 'Alexandra', 'arosse', 'ar@proconcept.ch');
-INSERT INTO timeplanner.utilisateur (id_user, nom, prenom, username, email) VALUES (3, 'Rossé', 'Christian', 'chrosse', 'chr@jura.ch');
-INSERT INTO timeplanner.utilisateur (id_user, nom, prenom, username, email) VALUES (4, 'Wüthrich', 'Cyril', 'awuth', 'aw@proconcept.ch');
+INSERT INTO timeplanner.utilisateur (id_utilisateur, nom, prenom, username, email,id_temps) VALUES (1, 'Rossé', 'Christophe', 'crosse', 'cr@proconcept.ch', NULL);
+INSERT INTO timeplanner.utilisateur (id_utilisateur, nom, prenom, username, email,id_temps) VALUES (2, 'Rossé', 'Alexandra', 'arosse', 'ar@proconcept.ch', NULL);
+INSERT INTO timeplanner.utilisateur (id_utilisateur, nom, prenom, username, email,id_temps) VALUES (3, 'Rossé', 'Christian', 'chrosse', 'chr@jura.ch', NULL);
+INSERT INTO timeplanner.utilisateur (id_utilisateur, nom, prenom, username, email, id_temps) VALUES (4, 'Wüthrich', 'Cyril', 'awuth', 'aw@proconcept.ch', NULL);
